0ca8c11ff85b642ac7a4a1de3eff129dabd3593e
Cody_Jones <cbjones93@gmail.com>
2021-03-05 15:37:33 -0600

    firstgiffy

---
+++ src/scripts/data/DataManager.js
@@ -0,0 +1,30 @@
+export const getUsers = () => {
+
+    return fetch("http://localhost:8088/users")
+    .then(response => response.json())
+}
+
+export const getPosts = () =>{
+
+    return fetch ("http://localhost:8088/posts")
+    .then(response => response.json())
+}
+const loggedInUser= {
+    id:1,
+    name:"bryan",
+    email:"bryan@home",
+}
+export const getLoggedInUser=()=>{
+    return loggedInUser;
+}
+
+export const getDadJoke = () =>{
+    return fetch ("https://icanhazdadjoke.com/",{
+        method:"GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    .then(response => response.json())
+    }
+  
---
+++ src/scripts/main.js
@@ -0,0 +1,41 @@
+import { getPosts, getDadJoke, getUsers} from "./data/DataManager.js";
+import { PostList } from "./feed/PostList.js";
+
+/**
+* Main logic module for what should happen on initial page load for Giffygram
+ */
+
+//Get a reference to the location on the DOM where the app will display
+// let postElement = document.querySelector(".postList");
+// let navElement = document.querySelector("nav");
+// let entryElement = document.querySelector(".entryForm")
+
+/*
+    This function performs one, specific task.
+
+    1. Can you explain what that task is?
+    2. Are you defining the function here or invoking it?
+*/
+// const startGiffyGram = () => {
+//     getUsers()
+//     .then(data=>{
+//         console.log("User Date", data)
+//     })
+// 	postElement.innerHTML = "Hello Cohort 47"
+// }
+// Are you defining the function here or invoking it?
+
+const showPostList = () => {
+    const postElement = document.querySelector(".postList");
+      getPosts().then((allPosts) => {
+          postElement.innerHTML = PostList(allPosts);
+      })
+  }
+  
+  
+  const startGiffyGram = () => 
+      showPostList();
+  
+  
+  startGiffyGram();
+getDadJoke();
